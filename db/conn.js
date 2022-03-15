0603f1eb39b9c19549a9f81bdbfd769af7c4dfba
Free Python Code <71100436+amr2018@users.noreply.github.com>
2022-03-15 20:57:34 +0200

    Update conn.js

--- db/conn.js
+++ db/conn.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-const connectURL = 'mongodb+srv://amr666:JBeUYVENUUyZ5nW@amrapps.fgbme.mongodb.net/tasks_app?retryWrites=true&w=majority'
+const connectURL = ''
 
 function connectDB () {
     return mongoose.connect(connectURL)
